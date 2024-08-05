cc9f64254623fd565a89f3e23e338ffd60687b05
Max Wittorf <maximilianwittorf@hotmail.de>
2024-08-05 22:21:22 +0200

    v.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.bmstudio.cra2go"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.121"
+        versionCode = 6
+        versionName = "0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
